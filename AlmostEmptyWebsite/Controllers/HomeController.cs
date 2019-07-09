075352a7494676a1f377aff5195d49225b02867a
Aaron Utterback <aar0nu@outlook.com>
2019-07-09 09:46:21 -0700

    Added Home Controller

---
+++ AlmostEmptyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmptyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // An ASP.NET Action is a public method in controller class.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
